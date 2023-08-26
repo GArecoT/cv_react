a9cfa05c492f4093f166d57adfc5127697a36ad4
Gabriel Areco <areco23@gmail.com>
2023-08-26 19:28:01 -0400

    Added: navbar and home page

--- cv_react/src/components/navbar/index.js
+++ cv_react/src/components/navbar/index.js
@@ -1,12 +1,16 @@
+import './navbar.css'
 export default function Navbar() {
 	return (
-		<div className="navbar bg-base-100">
+		<div className="navbar bg-base-100 lg:bg-base-200 z-10 sticky">
+			<h1 className="ml-5 font-sans font-semibold text-primary text-lg hidden lg:flex">Curriculo</h1>
 			<div className="navbar-start">
 			</div>
-			<div className="navbar-center">
-				<li className="btn"><a>Home</a></li>
-				<li className="btn"><a>Portfolio</a></li>
-				<li className="btn"><a>About</a></li>			</div>
+			<div className="navbar-center hidden lg:flex  ">
+				<h1 className="ml-5 font-sans font-semibold text-primary text-lg lg:hidden">Curriculo</h1>
+				<li className="btn btn-ghost"><a>Home</a></li>
+				<li className="btn btn-ghost"><a>Portfolio</a></li>
+				<li className="btn btn-ghost"><a>About</a></li>
+			</div>
 			<div className="navbar-end">
 			</div>
 		</div>
--- cv_react/src/index.js
+++ cv_react/src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Navbar from './components/navbar';
+import BottomNavbar from './components/BottomNavbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <html data-theme="dracula">
+      <Navbar></Navbar>
+      <App />
+      <BottomNavbar></BottomNavbar>
+    </html>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
