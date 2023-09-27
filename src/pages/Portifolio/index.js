8f1dd432329f2e9345e2ed7e607c9d7b03bda27e
Gabriel Areco Taveira <estagio.startup06@sesims.com.br>
2023-09-27 13:46:55 -0400

    FEATURE: finished portifolio page

--- src/pages/Portifolio/index.js
+++ src/pages/Portifolio/index.js
@@ -10,10 +10,12 @@ const Portifolio = () => {
           <div className="flex flex-col lg:flex-row place-items-start w-full">
             <div className="place-items-center w-full sm:w-[50%]">
               <p className="mb-5 font-thin">2019 ~ 2020</p>
-              <img
-                src="logo-thimalu.png"
-                className="bg-white p-5 rounded-3xl w-full"
-              />
+              <a href="https://www.thimalu.com.br" target="_blank">
+                <img
+                  src="logo-thimalu.png"
+                  className="bg-white p-5 rounded-3xl w-full hover:scale-[102%] transition"
+                />
+              </a>
             </div>
             <div className="place-items-center w-full lg:w-[150%]">
               <p className="text-lg lg:px-10 py-5 lg:py-14 text-justify">
@@ -30,74 +32,43 @@ const Portifolio = () => {
           <div className="divider mt-10" />
           <div className="flex flex-col lg:flex-row place-items-start w-full my-10">
             <div className="place-items-center w-full sm:w-[50%]">
-              <p className="mb-5 font-thin">2019 ~ 2020</p>
-              <img
-                src="logo-startup.png"
-                className="rounded-3xl w-full"
-              />
+              <p className="mb-5 font-thin">2022 ~ 2024</p>
+              <a
+                href="https://www.instagram.com/startupsesifiems/"
+                target="_blank"
+              >
+                <img
+                  src="logo-startup.png"
+                  className="rounded-3xl w-full hover:scale-[102%] transition"
+                />
+              </a>
             </div>
             <div className="place-items-center w-full lg:w-[150%]">
-              <p className="text-lg lg:px-10 py-5 lg:py-14 text-justify">
-                Trabalhei por 1 ano na Thimalu, com{" "}
-                <em>Google Ads</em>. Foi uma ótima experiência, aprendi a
-                trabalhar em equipe, como me organizar, organizar meu dinheiro.
-                Geri o{" "}
-                <em>site</em>. Fiquei encarregado dos estoques, cálculo de frete
-                e atendimento aos clientes <em>online</em>.
+              <p className="text-lg lg:px-10 pt-5 pb-3 lg:pb-5 lg:pt-10 text-justify">
+                Fui contratado em 2022 como estágiário na Startup Sesi. Comecei
+                desenvolvendo soluções IOT, e protótipos utlizando{" "}
+                <em>Arduino</em>. Trabalhei na <em>Startup</em>{" "}
+                por 2 anos, saindo após o fim do meu segundo ano como
+                estágiário.
+              </p>
+              <p className="text-lg lg:px-10 pb-3 lg:pb-5 text-justify">
+                Logo comecei a programar em{" "}
+                <em>front end</em>. Trabalhei em projetos em <em>vue</em>, com
+                {" "}
+                <em>bootstrap</em>. Depois comecei mexer com{" "}
+                <em>React Native</em> e{" "}
+                <em>NextJS</em>. Tive experiência com projetos reais, e
+                lançados, fazendo partes de times. Trabalhei, também, com
+                construção de UI na Unity.
+              </p>
+              <p className="text-lg lg:px-10 text-justify">
+                Durante meu tempo na{" "}
+                <em>Startup</em>, trabalhei usando metodologia <em>scrum</em>.
               </p>
             </div>
           </div>
         </div>
       </div>
-      {
-        /*
-      <div className="flex flex-row w-full mt-10">
-        <div className="h-[100vh] w-2 rounded-xl bg-gradient-to-b from-accent to-primary mr-3">
-        </div>
-        <div className="flex flex-col w-2/2 h-fit lg:flex-row mt-10">
-          <div className="grid m-2 place-items-start">
-            <div>
-              <p className="mb-5 font-thin">2019 ~ 2020</p>
-              <img
-                src="logo-thimalu.png"
-                className="bg-white p-5 rounded-3xl w-full"
-              />
-            </div>
-          </div>
-          <div className="grid m-2 place-items-start text-justify w-3/4">
-            <p className="text-lg lg:px-10 py-5 lg:py-14 text-justify">
-              Trabalhei por 1 ano na Thimalu, com{" "}
-              <em>Google Ads</em>. Foi uma ótima experiência, aprendi a
-              trabalhar em equipe, como me organizar, organizar meu dinheiro,
-              etc. Geri o{" "}
-              <em>site</em>, e fiquei encarregado dos estoques também.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row w-full">
-          <div className="flex flex-col w-2/2 h-fit lg:flex-row mt-10">
-            <div className="grid m-2 place-items-start">
-              <div>
-                <p className="mb-5 font-thin">2019 ~ 2020</p>
-                <img
-                  src="logo-thimalu.png"
-                  className="bg-white p-5 rounded-3xl w-full"
-                />
-              </div>
-            </div>
-            <div className="grid m-2 place-items-start text-justify w-3/4">
-              <p className="text-lg lg:px-10 py-5 lg:py-14 text-justify">
-                Trabalhei por 1 ano na Thimalu, com{" "}
-                <em>Google Ads</em>. Foi uma ótima experiência, aprendi a
-                trabalhar em equipe, como me organizar, organizar meu dinheiro,
-                etc. Geri o{" "}
-                <em>site</em>, e fiquei encarregado dos estoques também.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>*/
-      }
     </div>
   );
 };
